67d55bede2cda3902a3912b78d57204734482f9d
sallyliviakosasih <sallyliviak.1807@gmail.com>
2023-11-27 01:19:54 +0700

    Commit Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -43,50 +43,6 @@ export default function Header ({isLogged}) {
                             <a href='#' className='bg-primary-yellow text-contrast rounded-full font-medium px-4 py-2 mx-5'>Masuk</a>
                             <a href='#' className='border-2 rounded-full font-medium border-contrast px-4 py-2 mx-5'>Daftar</a>
                         </div>
-                        <Modal isOpen={modalLoginIsOpen} onRequestClose={toogleModalLogin} contentLabel="Pop Up Form">
-                            <div className='bg-white flex flex-col items-center justify-center'>
-                                    <Image className='mx-auto'
-                                        src={'/logo/beeOnly.svg'}
-                                        alt='Logo Learn Bee'
-                                        width={67}
-                                        height={68} />
-                                    <Image className='mx-auto my-2'
-                                        src={'/logo/textOnly.svg'}
-                                        alt='Logo 2 Learn Bee'
-                                        width={139}
-                                        height={23} />
-                                    <h2 className='text-center font-serif font-medium text-contrast text-3xl my-4'>Masuk</h2>
-                                    <form className='my-5'>
-                                        <label className='block my-2'>Email USU Mahasiswa</label>
-                                        <input type='text' className='w-full border-b-2'></input>
-                                        <label className='block my-2'>Kata Sandi</label>
-                                        <input type='text' className='w-full border-b-2'></input>
-                                    </form>
-                                    <button className='bg-primary-yellow text-contrast px-4 py-2 rounded-full block mx-auto my-16'>Masuk</button>
-                                </div> 
-                        </Modal>
-                        {/* <div className='fixed flex top-0 left-0 right-0 h-full z-10 bg-dark-muted/50 items-center justify-center' id='loginPopUpBg' tabIndex={-1}>
-                                <div className='bg-white relative rounded-lg p-12 w-4/12' id='loginFormContianer'>
-                                    <Image className='mx-auto'
-                                        src={'/logo/beeOnly.svg'}
-                                        alt='Logo Learn Bee'
-                                        width={67}
-                                        height={68} />
-                                    <Image className='mx-auto my-2'
-                                        src={'/logo/textOnly.svg'}
-                                        alt='Logo 2 Learn Bee'
-                                        width={139}
-                                        height={23} />
-                                    <h2 className='text-center font-serif font-medium text-contrast text-3xl my-4'>Masuk</h2>
-                                    <form className='my-5'>
-                                        <label className='block my-2'>Email USU Mahasiswa</label>
-                                        <input type='text' className='w-full border-b-2'></input>
-                                        <label className='block my-2'>Kata Sandi</label>
-                                        <input type='text' className='w-full border-b-2'></input>
-                                    </form>
-                                    <button className='bg-primary-yellow text-contrast px-4 py-2 rounded-full block mx-auto my-16'>Masuk</button>
-                                </div>
-                            </div> */}
                         </>
                     }
                     {isLogged &&
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
-export default function Post() {
+export default function Post({id, votes, authorName, jurusan, stambuk, waktu, title, content, images}) {
     return (
         <div className='border border-dark-muted my-8 px-6 py-5'>
             <div className='flex gap-5' id='headerQuestin'>
@@ -12,27 +12,30 @@ export default function Post() {
                         height={29}
                     />
                     </button>
-                    <h1 className='text-center text-lg'>100</h1>
+                    <h1 className='text-center text-lg'>{votes}</h1>
                 </div>
                 <div className='grid grid-rows-2' id='uploaderDetail'>
                     <div className='flex gap-4 items-center' id='userDetail'>
-                    <h1 className='font-bold text-lg'>Adi Taher</h1>
+                    <h1 className='font-bold text-lg'>{authorName}</h1>
                     <div className='flex gap-1 text-sm text-darker-muted'>
-                        <h2>2020</h2>
-                        <h2>Ilmu Komputer</h2>
+                        <h2>{stambuk}</h2>
+                        <h2>{jurusan}</h2>
                     </div>
                     </div>
-                    <h2 className='text-sm text-darker-muted' id='time'>1 menit yang lalu</h2>
+                    <h2 className='text-sm text-darker-muted' id='time'>{waktu}</h2>
                 </div>
             </div>
             <div className='py-4' id='mainQuestion'>
-                <h1 className='font-bold text-lg'>Tolong bantu saya untuk memecahkan error berikut!!</h1>
-                <Image className='mx-auto my-3'
-                    src={'/sample/sample1.png'}
+                <h1 className='font-bold text-lg'>{title}</h1>
+                {images!=null && <Image className='mx-auto my-3'
+                    src={`${images}`}
                     alt='Gambar Soal'
                     width={820}
                     height={320}
-                />
+                />}
+                {content!=null && 
+                    <h2>{content}</h2>
+                }
             </div>
             <div className='flex relative items-center' id='QuestionDetail'>
                 <div className='flex px-4 gap-3' id='tags'>
